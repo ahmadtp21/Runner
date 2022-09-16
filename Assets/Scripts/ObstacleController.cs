7da93247a1acf342451a5ab0a74e56d6358be624
Mad <113595329+ahmadtp21@users.noreply.github.com>
2022-09-16 13:10:12 +0700

    Import project

---
+++ Assets/Scripts/ObstacleController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour {
+
+	public float force;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.AddForce(new Vector3(-force, 0.0f, 0.0f), ForceMode.Impulse);
+	}
+}
